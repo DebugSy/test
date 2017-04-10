fc79d4c504271f714d6213253986cbc1b12254f3
DebugSy <532151398@qq.com>
2017-04-10 16:09:51 +0800

    add branch fix-B

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,8 +4,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("i : " + i);
-        }
+        System.out.println("add branch fix-B");
     }
 }
